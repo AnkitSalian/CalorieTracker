05dda40059c3e75b11f0f308dff80ab09ea6544e
Ankit Salian <ankitsalian9@gmail.com>
2021-08-21 22:26:44 +0530

    Basic skeleton for backend

---
+++ server/router/calorie.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+// const { protect } = require('../middleware/auth');
+
+// router.post('/calorie/add', protect, addFood);
+
+// router.patch('/calorie/:id', protect, updateFood);
+
+// router.delete('/calorie/:id', protect, deleteFood);
+
+//router.get('/calorie', protect, fetchFood );
+
+module.exports = router;
